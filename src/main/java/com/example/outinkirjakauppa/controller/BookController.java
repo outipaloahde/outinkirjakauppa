4c9cf2031f5239eea3629712d0878f675654f85d
outipaloahde <outipaloahde@gmail.com>
2018-01-31 19:12:43 +0200

    ekat lisäykset

---
+++ src/main/java/com/example/outinkirjakauppa/controller/BookController.java
@@ -0,0 +1,17 @@
+package com.example.outinkirjakauppa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	
+	@RequestMapping("/index")
+	public String home(){
+		return "index";
+	}
+	
+	
+	
+
+}
