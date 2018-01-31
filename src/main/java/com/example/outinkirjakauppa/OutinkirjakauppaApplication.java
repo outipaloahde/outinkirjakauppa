46e9f657f80b32f566b54b58100aa2277a89f760
outipaloahde <outipaloahde@gmail.com>
2018-01-31 15:18:33 +0200

    first

---
+++ src/main/java/com/example/outinkirjakauppa/OutinkirjakauppaApplication.java
@@ -0,0 +1,12 @@
+package com.example.outinkirjakauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutinkirjakauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutinkirjakauppaApplication.class, args);
+	}
+}
