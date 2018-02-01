0a09089f3adb574ef14c2b22290e9ccfe0124803
outipaloahde <outipaloahde@gmail.com>
2018-02-01 13:17:20 +0200

    add + delete

--- src/main/java/com/example/outinkirjakauppa/controller/BookController.java
+++ src/main/java/com/example/outinkirjakauppa/controller/BookController.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.outinkirjakauppa.domain.Book;
@@ -27,6 +29,23 @@ public class BookController {
 		model.addAttribute("list", repository.findAll());
 	
 		return "booklist";
+	}
+	
+	@RequestMapping("/add")
+	public String add(Model model){
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping("/save")
+	public String save(Book book){
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+	public String delete(@PathVariable("id") Long id, Model model){
+		repository.delete(id);
+		return "redirect:../booklist";
 	}
 	
 }
