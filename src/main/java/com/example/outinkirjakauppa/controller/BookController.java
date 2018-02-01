f8569dea035bb3d86543d9af0362c2d6d40d2f1b
outipaloahde <outipaloahde@gmail.com>
2018-02-01 12:22:38 +0200

    listpage

--- src/main/java/com/example/outinkirjakauppa/controller/BookController.java
+++ src/main/java/com/example/outinkirjakauppa/controller/BookController.java
@@ -1,15 +1,21 @@
 package com.example.outinkirjakauppa.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.outinkirjakauppa.domain.Book;
 import com.example.outinkirjakauppa.domain.BookRepository;
 
 @Controller
 public class BookController {
 	@Autowired
 	private BookRepository repository;
+
 	
 	@RequestMapping("/index")
 	public String home(){
@@ -17,15 +23,10 @@ public class BookController {
 	}
 	
 	@RequestMapping("/booklist")
-	public String listofbooks(){
-		
-		
-		
-		
+	public String listofbooks(Model model){
+		model.addAttribute("list", repository.findAll());
+	
 		return "booklist";
 	}
 	
-	
-	
-
 }
--- src/main/java/com/example/outinkirjakauppa/domain/BookRepository.java
+++ src/main/java/com/example/outinkirjakauppa/domain/BookRepository.java
@@ -5,6 +5,6 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long>{
-	List<Book> findByAuthor(String author);
+
 
 }
